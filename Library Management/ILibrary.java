716c21bb73b168d25be9f6f0572066d80fc421f7
HARIHARAN2209 <ahariharan0922@gmail.com>
2025-02-24 08:31:39 +0530

    dd

--- Library Management/ILibrary.java
+++
@@ -1,9 +0,0 @@
-package libraryManagementSystem;
-
-public interface ILibrary {
-	void borrowBook(String ISBN, String userID) throws BookNotFoundException, UserNotFoundException, MaxBooksAllowedException;
-	void returnBook(String ISBN, String userID) throws BookNotFoundException, UserNotFoundException;
-	void reserveBook(String ISBN, String userID) throws BookNotFoundException, UserNotFoundException;
-	Book searchBook(String title);
-
-}
--- WareHouseInventoryMangement/OrderComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-class OrderComparator implements Comparator<Order> {
-    @Override
-    public int compare(Order a, Order b) {
-        return b.getPriority().compareTo(a.getPriority());
-    }
-}
